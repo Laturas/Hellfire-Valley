876227382eab3d531d83e4420e360c1d3ad1b7f2
Laturas <laturaas@gmail.com>
2025-02-22 15:38:33 -0500

    game control singleton update and crop selling

--- Assets/Scripts/Player Movement/ThingPlacer.cs
+++ Assets/Scripts/Player Movement/ThingPlacer.cs
@@ -20,12 +20,12 @@ public class ThingPlacer : MonoBehaviour
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            ChangeObjectToPlace(SOManager.instance.placeables[0]);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            ChangeObjectToPlace(SOManager.instance.placeables[1]);
-        }
+        // if (Input.GetKeyDown(KeyCode.Alpha1)) {
+        //     ChangeObjectToPlace(SOManager.instance.placeables[0]);
+        // }
+        // if (Input.GetKeyDown(KeyCode.Alpha2)) {
+        //     ChangeObjectToPlace(SOManager.instance.placeables[1]);
+        // }
         if (currentPlaceable == null) {
             return;
         }
